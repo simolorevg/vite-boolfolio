dabb9043f0f30d8f25228f9c0a11c8e071e0fe9a
simolorevg <simoneviaggi@libero.it>
2023-06-13 16:10:33 +0200

    Made the axios get call

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,23 @@
-<script setup>
-
+<script>
+import { store } from './store';
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      store
+    }
+  },
+  mounthed() {
+    this.callProjects();
+  },
+  methods: {
+    callProjects() {
+      axios.get(this.store.localUrl).then((resp) => {
+        this.store.projects = resp.data.results;
+      })
+    }
+  }
+}
 </script>
 
 <template></template>
