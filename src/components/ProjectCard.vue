6cdcf0951385cc450f928400e717e2f9ded70cd9
simolorevg <simoneviaggi@libero.it>
2023-06-14 18:13:55 +0200

    This is my best. Is not too much...but his an onest work

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -9,16 +9,27 @@ export default {
 }
 </script>
 <template>
-    <div class="card h-100">
-        <div class="card-body">
-            <h5>{{ project.title }}</h5>
+    <div class="my-card h-100 d-flex flex-column">
+        <div class="my-card-image">
+            <div class="img-unavaiable">
+                SORRY THE IMG IS NOT AVAIABLE :-(
+            </div>
+        </div>
+        <div class="my-card-body">
+            <h5>{{ project.title }} <span>
+                    <p> di {{ project.author }}</p>
+                </span></h5>
             <p>{{ project.post }}</p>
-            <p>{{ project.author }}</p>
-            <h5>Tecnologie</h5>
-            <ul v-for="tech in project.technologies" :key="project.technologies.id">
-                <li>{{ tech.name }}</li>
-            </ul>
+            <div v-if="project.technologies !== undefined">
+                <h5>Tecnologie</h5>
+                <p>
+                    <span v-for="tech in project.technologies" :key="project.technologies.id">{{ tech.name }}, </span>
+                </p>
+            </div>
+            <div v-else>
+                <h6>Nessuna tecnologia</h6>
+            </div>
         </div>
     </div>
 </template>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>@use '../style/general.scss' as *;</style>
