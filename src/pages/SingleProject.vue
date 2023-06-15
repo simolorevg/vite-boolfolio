761beead6ef725e58289852b7657a04376e680b5
simolorevg <simoneviaggi@libero.it>
2023-06-15 17:15:52 +0200

    Working on the single project page

--- src/pages/SingleProject.vue
+++ src/pages/SingleProject.vue
@@ -1,6 +1,26 @@
 <script>
+import axios from 'axios';
+import { store } from '../store';
+
 export default {
-    name: 'SinglePost'
+    name: 'SinglePost',
+    data() {
+        return {
+            store,
+            project: null,
+            errorMsg: ""
+        }
+    },
+    mounted() {
+        const idParam = this.$route.params.id;
+        console.log('id numero ', idParam);
+        axios.get(`http://127.0.0.1:8000/api/projects/${idParam}`).then((resp) => {
+            if (resp.data.success) {
+                this.project = resp.data.results;
+                console.log(this.project);
+            }
+        });
+    }
 }
 </script>
 <template></template>
