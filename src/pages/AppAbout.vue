71609c12befe196490af6cb0fb80581d492ee91d
simolorevg <simoneviaggi@libero.it>
2023-06-19 17:23:19 +0200

    A little surprise for you

--- src/pages/AppAbout.vue
+++ src/pages/AppAbout.vue
@@ -4,6 +4,58 @@ export default {
 }
 </script>
 <template>
-    <h2 class="text-center">About page</h2>
+    <h2 class="text-center">E siamo qua...</h2>
+    <div class="container">
+        <p>
+            Oggi mi trovo qui con il cuore in gola (in senso metaforico, non serve chiamare un'ambulanza), poiché siamo
+            giunti alla fine di questa avventura. È stato un percorso intenso, ricco di sfide e
+            conquiste: con un contorno di risate.
+
+            Prima di tutto, desidero ringraziare i NOSTRI tutor,Roberto e Paolo, e la NOSTRA
+            insegnante, Olga. Grazie per averci guidato lungo questo percorso. Le vostre
+            competenze
+            tecniche e la vostra passione per la programmazione web ci hanno ispirato e ci hanno spinti a dare il massimo in
+            ogni lezione. Grazie per averci dato le ali per volare nel vasto mondo della tecnologia (non pensavo di essere
+            cosi filosofico, wow).
+        </p>
+        <p>
+            Ringrazio i miei fantastici compagni: in sei mesi da perfetti sconosciuti, a una legione di
+            paperelle pronte ad affrontare qualsiasi sfida (tranne il carosello: ho ancora gli incubi la notte).
+            Abbiamo condiviso le nostre sfide, le nostre difficoltà e le nostre vittorie. Il nostro spirito
+            di
+            squadra ha reso questo percorso ancora più prezioso. Ognuno di noi ha apportato un contributo unico al
+            gruppo, con le proprie competenze e la propria creatività: anche con il senso dell'umorismo. Sono contento di
+            aver passato
+            ore di studio, programmazione e
+            risoluzione di problemi (rischiando anche la scomunica dal Papa più di una volta) assieme.
+            Questo percorso non sarebbe stato lo stesso senza di voi.
+        </p>
+        <p>
+            Oggi, guardandomi indietro, posso affermare due cose: la prima è che togliero quella "maledetta" maglietta del
+            Milan (promesso, croce sul cuore) e la seconda è che questa esperienza è stata molto più di un
+            corso di programmazione web, è stata un'avventura che ha arricchito le nostre vite in molti modi. Abbiamo
+            acquisito
+            competenze tecniche che ci renderanno competitivi nel mondo del lavoro, ma abbiamo anche imparato l'importanza
+            della
+            perseveranza, della collaborazione e della flessibilità mentale.
+        </p>
+        <p>
+            Queste lezioni non svaniranno mai (Google Drive
+            permettendo), e porteremo
+            con
+            noi le nostre esperienze nel nostro percorso futuro.
+        </p>
+        <p>
+            Infine, vorrei dedicare un ringraziamento speciale a ognuno di voi per aver creduto in me e per avermi sostenuto
+            personalmente. Le vostre parole gentili, il vostro incoraggiamento e la vostra amicizia hanno reso questa
+            avventura
+            ancora più speciale. Sono grato di aver conosciuto persone così straordinarie e spero che le nostre strade si
+            incrocino ancora in futuro. Chissà lavoreremo assieme: DIPENDE.
+
+            In conclusione, vi ringrazio di cuore per questi cinque mesi di avventura nel mondo della programmazione.
+        </p>
+        <p>Vostro</p>
+        <p>Viaggi Simone Lorenzo, classe 90</p>
+    </div>
 </template>
 <style lang="scss"></style>
